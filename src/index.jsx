202e55937a1acd00485511a35b1dc3f0a2a77765
Jim Palowski <palowskijim@gmail.com>
2018-06-25 15:44:06 -0700

    Add tickets

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 var heading = React.createElement('h1', {}, 'Help Queue');
 var ticketLocation = React.createElement('h3', {}, '3a');
@@ -8,11 +9,6 @@ var ticketIssue = React.createElement('h3', {}, "Firebase won't save record");
 var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
 ReactDOM.render(
-  <div>
-    <h1>Help Queue</h1>
-    <h3>3a</h3>
-    <h3>Thato and Haley</h3>
-    <p><em>Firebase will not save record!</em></p>
-  </div>,
+  <App/>,
   document.getElementById('react-app-root')
 );
