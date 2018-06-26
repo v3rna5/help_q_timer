7b11f3e616b9c68bda8649f031c92f1c0a1a202f
Jim Palowski <palowskijim@gmail.com>
2018-06-26 14:46:38 -0700

    Add function call

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 
 function Ticket(){
   return (
-      <div>
-        <style global jsx>{`
+    <div>
+      <style global jsx>{`
           div {
             background-color: red;
           }
         `}</style>
-        <h3>3A - Thato & Haley</h3>
-        <p><em>Firebase will not save record!</em></p>
-        <hr/>
-      </div>
-  );
+      <h3>3A - Thato & Haley</h3>
+      <p><em>Firebase will not save record!</em></p>
+      <hr/>
+    </div>
+  )
 }
 
-export default Ticket;
+export default Ticket
