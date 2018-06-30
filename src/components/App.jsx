fd90a5f438214eedd120f5fec5fb00b5e72c42c4
Jim Palowski <palowskijim@gmail.com>
2018-06-30 13:37:36 -0700

    Add Error route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import TicketList from './TicketList'
 import { Switch, Route } from 'react-router-dom';
 import NewTicketForm from './NewTicketForm';
+import Error404 from './Error404';
 
 
 function App() {
@@ -31,6 +32,8 @@ function App() {
       <Switch>
       <Route exact path='/' component={TicketList} />
       <Route path='/newticket' component={NewTicketForm} />
+      <Route component={Error404} />
+
       </Switch>
     </div>
   )
